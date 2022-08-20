3ec3259342931888e855da62e83efad54a555b03
Lim Sung Hyuk <limbit@naver.com>
2022-08-20 11:02:15 +0900

    first

---
+++ src/Gittest.java
@@ -0,0 +1,15 @@
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("Git test");
+        System.out.println("git test2");
+        System.out.println("git test2");
+        System.out.println("git test2");
+        System.out.println("git test2");
+        System.out.println("git test2");
+        System.out.println("git test2");
+        System.out.println("git test2");
+        System.out.println("git test2");
+        System.out.println("git test2");
+        System.out.println("git test2");
+    }
+}
---
+++ src/gittest2.java
@@ -0,0 +1,11 @@
+public class gittest2 {
+    public static void main(String[] args) {
+        System.out.println("git test2");
+        System.out.println("git test2");
+        System.out.println("git test2");
+        System.out.println("git test2");
+        System.out.println("git test2");
+
+    }
+
+}
